7283a393001d48d49c448bd4d6f0be27e9857d64
murat12313 <139007286+murat12313@users.noreply.github.com>
2026-03-06 00:00:08 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:00:03.410Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T21:00:05.158Z
+Commit: Patch API endpoint error handling
