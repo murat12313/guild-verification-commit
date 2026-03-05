0db96ce3c171e695791e34c59a46c5dbca403b08
murat12313 <139007286+murat12313@users.noreply.github.com>
2026-03-06 00:00:24 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:00:07.997Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T21:00:21.060Z
+Commit: Fix broken links in documentation
